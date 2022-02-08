8ac775e09eba9b6ea2ae750b0bda91168ec170e5
Andre <aa.andremacedo@gmail.com>
2022-02-08 18:26:12 -0300

    acrescentados os desafios 7 8 e 9

--- src/challenges.js
+++ src/challenges.js
@@ -70,23 +70,116 @@ function highestCount(array) {
 // console.log(highestCount( [0, 0, 0]));
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+
+function catAndMouse(cat1, cat2) {
+  if (cat1 < cat2) {
+    return 'cat2'
+  } else if (cat2 < cat1) {
+    return 'cat1'
+  } else {
+    return 'os gatos trombam e o rato foge'
+  }
 }
+
+
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  // let div3 = [];
+  // let div5 = [];
+  let result = [];
+  let is3;
+  let is5;
+  for (let index in array) {
+    is3 = false;
+    is5 = false;
+    // console.log(array[index])
+    if (array[index] % 3 == 0) {
+      // div3.push(array[index]);
+      is3 = true;
+    }
+    if (array[index] % 5 == 0) {
+      // div5.push(array[index]);
+      is5 = true;
+    }
+
+    if (is3 && is5) {
+      result.push("fizzBuzz")
+    } else if (!is3 && !is5) {
+      result.push("bug!")
+    } else if (is3) {
+      result.push("fizz")
+    } else if (is5) {
+      result.push("buzz")
+    }
+  }
+  // console.log(div3)
+  // console.log(div5)
+  return result;
 }
+// console.log(fizzBuzz([2, 15, 7, 9, 45]))
+// console.log(fizzBuzz([7, 9]))
+// console.log(fizzBuzz([9, 25]))
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(param) {
+  response = ''
+  let string = param.split('')
+  for (let i in string) {
+    switch (string[i]) {
+      case 'a':
+        response = response + 1;
+        break;
+      case 'e':
+        response = response + 2;
+        break;
+      case 'i':
+        response = response + 3;
+        break;
+      case 'o':
+        response = response + 4;
+        break;
+      case 'u':
+        response = response + 5;
+        break;
+      default:
+        response = response + string[i];
+        break;
+    }
+  }
+  return response;
 }
 
-function decode() {
-  // seu código aqui
+function decode(param) {
+  response = ''
+  let string = param.split('')
+  for (let i in string) {
+    switch (string[i]) {
+      case '1':
+        response = response + 'a';
+        break;
+      case '2':
+        response = response + 'e';
+        break;
+      case '3':
+        response = response + 'i';
+        break;
+      case '4':
+        response = response + 'o';
+        break;
+      case '5':
+        response = response + 'u';
+        break;
+      default:
+        response = response + string[i];
+        break;
+    }
+  }
+  return response;
 }
+
+// console.log(encode("hi there!"))
+// console.log(decode(encode("hi there!")))
 
 // Desafio 10
 function techList() {
