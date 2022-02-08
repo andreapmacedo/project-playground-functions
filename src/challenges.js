ed388d125b8303ec91150d70565e9a4cca48d18a
Andre <aa.andremacedo@gmail.com>
2022-02-08 20:57:26 -0300

    corrigido o desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -6,100 +6,69 @@ function compareTrue(param1, param2) {
   return false;
 
 }
-// console.log(compareTrue(true, true))
-// console.log(compareTrue(true, false))
-// console.log(compareTrue(false, false))
-
 
 // Desafio 2
 function calcArea(base, height) {
-  // let response;
-  // response = (base * height)/2; 
-  // return response;
   return (base * height) / 2;
 }
-
-// console.log(calcArea(10, 50))
-
 
 // Desafio 3
 function splitSentence(sentence) {
   return sentence.split(' ');
 }
 
-// console.log(splitSentence('go Trybe'))
-
 // Desafio 4
 function concatName(array) {
   return array[array.length - 1] + ', ' + array[0];
 }
-
-// let arrayD4 = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
-// console.log(concatName(arrayD4)); 
-
 
 // Desafio 5
 function footballPoints(wins, ties) {
   return (wins * 3) + (ties * 1);
 }
 
-// console.log(footballPoints(14, 8))
-
-// Desafio 6
+//Desafio 6
 function highestCount(array) {
-  let tempD6;
-  let sumD6 = 1;
+  let temp;
+  let sum = 1;
   for (let index = 0; index < array.length; index++) {
     if (index == 0) {
-      tempD6 = array[0];
-    } else if (array[index] >= tempD6) {
-      // console.log(index);
-      // console.log(index, array[index]);
-      if (array[index] > tempD6) {
-        tempD6 = array[index];
-        sumD6 = 1;
+      temp = array[0];
+    } else if (array[index] >= temp) {
+      if (array[index] > temp) {
+        temp = array[index];
+        sum = 1;
       } else {
-        sumD6++;
+        sum++;
       }
     }
   }
-  return sumD6;
+  return sum;
 }
-// console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
-// console.log(highestCount([0, 4, 4, 4, 9, 2, 1]));
-// console.log(highestCount( [0, 0, 0]));
 
 // Desafio 7
-
-function catAndMouse(cat1, cat2) {
-  if (cat1 < cat2) {
-    return 'cat2'
-  } else if (cat2 < cat1) {
-    return 'cat1'
-  } else {
+function catAndMouse(mouse, cat1, cat2) {
+  if((cat2-mouse) === (mouse-cat1)){
     return 'os gatos trombam e o rato foge'
+  }else if((cat2-mouse) < (cat1-mouse)){
+    return 'cat2'
+  }else {
+    return 'cat1'
   }
 }
 
-
-
 // Desafio 8
-function fizzBuzz(array) {
-  // let div3 = [];
-  // let div5 = [];
+function fizzBuzz(array) {  
   let result = [];
   let is3;
   let is5;
   for (let index in array) {
     is3 = false;
     is5 = false;
-    // console.log(array[index])
     if (array[index] % 3 == 0) {
-      // div3.push(array[index]);
       is3 = true;
     }
     if (array[index] % 5 == 0) {
-      // div5.push(array[index]);
       is5 = true;
     }
 
@@ -113,13 +82,8 @@ function fizzBuzz(array) {
       result.push("buzz")
     }
   }
-  // console.log(div3)
-  // console.log(div5)
   return result;
 }
-// console.log(fizzBuzz([2, 15, 7, 9, 45]))
-// console.log(fizzBuzz([7, 9]))
-// console.log(fizzBuzz([9, 25]))
 
 // Desafio 9
 function encode(param) {
@@ -177,9 +141,6 @@ function decode(param) {
   }
   return response;
 }
-
-// console.log(encode("hi there!"))
-// console.log(decode(encode("hi there!")))
 
 // Desafio 10
 function techList() {
